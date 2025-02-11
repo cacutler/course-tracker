7b3ef76667182a5a0bc8871f0aae1423cc9f03c4
cacutler <116305038+cacutler@users.noreply.github.com>
2025-02-10 21:52:28 -0700

    Updated code styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase project is stored on calexcutler@gmail.com account
 const firebaseConfig = {
     apiKey: "AIzaSyDbrIlmll4W3jW0GLCSNjGzkXXaWqrq6M8",
     authDomain: "course-tracker-f892d.firebaseapp.com",
@@ -15,7 +15,6 @@ const firebaseConfig = {
     appId: "1:818383863033:web:62fd223e205560b657b628",
     measurementId: "G-3FQ5NCEW0R"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
