ecb596370aa2c068b2a0fd1c318f3ef20d97f93c
cacutler <116305038+cacutler@users.noreply.github.com>
2025-02-19 13:21:58 -0700

    Generated login and registration form components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginFormComponent, RegistrationFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
