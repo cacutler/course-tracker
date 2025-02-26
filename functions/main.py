803c13be4cea8756ebce7ec8b84dd714d5bb37dd
cacutler <116305038+cacutler@users.noreply.github.com>
2025-02-25 18:34:25 -0700

    Styling some code

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,9 @@
 # Welcome to Cloud Functions for Firebase for Python!
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
-
 initialize_app()
-
-
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
     if req.method == 'OPTIONS':
@@ -19,11 +15,9 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
             'Access-Control-Max-Age': '3600'
         }
         return https_fn.Response("", status=204, headers=headers)
-    
     # Set CORS headers for main requests
     headers = {
         'Access-Control-Allow-Origin': '*'
     }
-    
     # Your actual function logic
     return https_fn.Response("Hello world!", headers=headers)
