82309ddb93a30dacd6b2ae1166a4b6337c6fdea5
cacutler <116305038+cacutler@users.noreply.github.com>
2025-03-06 15:03:29 -0700

    Used a few ngIf statements to disable visibility of login and registration forms and sign out button

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css'
 })
 export class AppHeaderComponent {
-
+  authenticated: boolean = false;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,14 @@ import { FutureCoursesComponent } from './future-courses/future-courses.componen
 import { PassedCoursesComponent } from './passed-courses/passed-courses.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { PlansComponent } from './plans/plans.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [LoginFormComponent, RegistrationFormComponent, MajorDetailsComponent, AvailableCoursesComponent, FutureCoursesComponent, PassedCoursesComponent, AppHeaderComponent, PlansComponent],
+  imports: [LoginFormComponent, RegistrationFormComponent, MajorDetailsComponent, AvailableCoursesComponent, FutureCoursesComponent, PassedCoursesComponent, AppHeaderComponent, PlansComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'course-tracker';
+  title: string = 'course-tracker';
+  authenticated: boolean = false;
 }
